702960dc7bd8f80bc591d1d600b9599827219045
Shadowboss109 <theodorebennett3@gmail.com>
2022-08-10 11:42:19 -0500

    Continue to option screen

--- Bank.java
+++ Bank.java
@@ -4,71 +4,116 @@ import java.util.Scanner;
 public class Bank {
 
     private double balance;
+    private String select;
 
     public Bank() {
 
-        System.out.println("        WELCOME TO OUR ONLINE BANKING");
-        System.out.println("CHOOSE THE NUMBER FOR THE OPTION THAT BEST SUITS YOU:");
-        System.out.println("0. EXIT");
-        System.out.println("1. WITHDRAW");
-        System.out.println("2. DIPOSIT");
-        System.out.println("3. DISPLAY CURRENT BALANCE");
+        loopA:while (true) {
+            // Display bank options
+            System.out.println("        WELCOME TO OUR ONLINE BANKING");
+            System.out.println("CHOOSE THE NUMBER FOR THE OPTION THAT BEST SUITS YOU:");
+            System.out.println("0. EXIT");
+            System.out.println("1. WITHDRAW");
+            System.out.println("2. DIPOSIT");
+            System.out.println("3. DISPLAY CURRENT BALANCE");
 
-        Scanner in = new Scanner(System.in);
-        int option = in.nextInt();
+            // Select banking option
+            Scanner in = new Scanner(System.in);
+            int option = in.nextInt();
 
-        switch (option) {
-            case 0:
-                System.out.println("THANK YOU FOR BANKING US");
-                break;
+            switch (option) {
+                case 0:
+                    System.out.println("THANK YOU FOR BANKING US");
+                    break loopA;
 
-            case 1:
-                System.out.println("ENTER THE AMOUNT YOU WOULD LIKE TO WITHDRAW");
-                int withdraw_amount = in.nextInt();
-                in.nextLine();
-                if (withdraw_amount <= getBalance()) {
-                    withdraw(withdraw_amount);
+                case 1:
+                    System.out.println("ENTER THE AMOUNT YOU WOULD LIKE TO WITHDRAW");
+                    int withdraw_amount = in.nextInt();
+                    in.nextLine();
+                    if (withdraw_amount <= getBalance()) {
+                        withdraw(withdraw_amount);
+                        display();
+                        System.out.println(" ");
+                        moveOn();
+                        select = in.next();
+                        if (select.equals("y") || select.equals("Y")) {
+                            break;
+                        } else {
+                            break loopA;
+                        }
+
+                    } else {
+                        System.out.println("INSUFFICIENT BALANCE! ");
+                        break loopA;
+
+                    }
+
+                case 2:
+                    System.out.println("ENTER THE AMOUNT YOU WOULD LIKE TO DEPOSIT");
+                    int deposit_amount = in.nextInt();
+                    deposit(deposit_amount);
+                    in.nextLine();
                     display();
-                    break;
-                } else {
-                    System.out.println("INSUFFICIENT BALANCE! ");
+                    System.out.println(" ");
+                    moveOn();
+                    select = in.next();
+                    if (select.equals("y") || select.equals("Y")) {
+                        break;
+                    } else {
+                        break loopA;
+                    }
 
-                }
+                case 3:
+                    display();
+                    System.out.println(" ");
+                    moveOn();
+                    select = in.next();
+                    if (select.equals("y") || select.equals("Y")) {
+                        break;
+                    } else {
+                        break loopA;
+                    }
 
-            case 2:
-                System.out.println("ENTER THE AMOUNT YOU WOULD LIKE TO DEPOSIT");
-                int deposit_amount = in.nextInt();
-                deposit(deposit_amount);
-                in.nextLine();
-                display();
-                break;
-
-            case 3:
-                display();
-                break;
+                default:
+                    System.out.println("NOT A VALID OPTION");
+                    break loopA;
+            }
         }
 
     }
 
+    // Return bank balance
     public double getBalance() {
         return this.balance;
     }
 
+    // Deposit fund
     public void deposit(double balance) {
         this.balance += balance;
     }
+
+    // Withdraw fund
 
     public void withdraw(double balance) {
         this.balance -= balance;
     }
 
+    // Display balance
     public void display() {
         System.out.printf("YOUR CURRENT BALANCE IS %.2f", getBalance());
     }
 
+    // Continue online banking or end
+    public void moveOn() {
+        
+        System.out.println("DO YOU WISH TO CONTINUE ? ");
+        System.out.println("SELECT Y FOR YES");
+        System.out.println("SELECT N FOR NO");
+    }
+
     public static void main(String[] args) {
         Bank b = new Bank();
-        ;
+
     }
 
 }
