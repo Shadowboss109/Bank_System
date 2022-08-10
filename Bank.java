3356acc71f9e25814f4d5f3186948c39918e1dce
Shadowboss109 <theodorebennett3@gmail.com>
2022-08-10 10:51:57 -0500

    select options

--- Bank.java
+++ Bank.java
@@ -1,11 +1,54 @@
+
+import java.util.Scanner;
+
 public class Bank {
 
     private double balance;
 
-    public Bank(){
+    public Bank() {
+
+        System.out.println("        WELCOME TO OUR ONLINE BANKING");
+        System.out.println("CHOOSE THE NUMBER FOR THE OPTION THAT BEST SUITS YOU:");
+        System.out.println("0. EXIT");
+        System.out.println("1. WITHDRAW");
+        System.out.println("2. DIPOSIT");
+        System.out.println("3. DISPLAY CURRENT BALANCE");
+
+        Scanner in = new Scanner(System.in);
+        int option = in.nextInt();
+
+        switch (option) {
+            case 0:
+                System.out.println("THANK YOU FOR BANKING US");
+                break;
+
+            case 1:
+                System.out.println("ENTER THE AMOUNT YOU WOULD LIKE TO WITHDRAW");
+                int withdraw_amount = in.nextInt();
+                in.nextLine();
+                if (withdraw_amount <= getBalance()) {
+                    withdraw(withdraw_amount);
+                    display();
+                    break;
+                } else {
+                    System.out.println("INSUFFICIENT BALANCE! ");
+
+                }
+
+            case 2:
+                System.out.println("ENTER THE AMOUNT YOU WOULD LIKE TO DEPOSIT");
+                int deposit_amount = in.nextInt();
+                deposit(deposit_amount);
+                in.nextLine();
+                display();
+                break;
+
+            case 3:
+                display();
+                break;
+        }
 
     }
-
 
     public double getBalance() {
         return this.balance;
@@ -19,18 +62,13 @@ public class Bank {
         this.balance -= balance;
     }
 
-    public void display(){
-        System.out.printf("Your current balance is %.2f",getBalance());
+    public void display() {
+        System.out.printf("YOUR CURRENT BALANCE IS %.2f", getBalance());
     }
 
     public static void main(String[] args) {
-        Bank b=new Bank();
-        b.deposit(200);
-        b.withdraw(50);
-        b.display();
+        Bank b = new Bank();
+        ;
     }
 
-
-    
-    
 }
